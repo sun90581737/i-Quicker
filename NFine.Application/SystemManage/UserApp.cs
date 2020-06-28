905570b36ec214c4b40092ccc0a236913652b754
sunxuhong <sun905817@163.com>
2020-06-28 20:56:52 +0800

    vH6.28.2

--- NFine.Application/SystemManage/UserApp.cs
+++ NFine.Application/SystemManage/UserApp.cs
@@ -91,5 +91,35 @@ namespace NFine.Application.SystemManage
                 throw new Exception("账户不存在，请重新输入");
             }
         }
+
+        public UserEntity InitLogin(string username)
+        {
+            UserEntity userEntity = service.FindEntity(t => t.F_Account == username);
+            if (userEntity != null)
+            {
+                if (userEntity.F_EnabledMark == true)
+                {
+                    UserLogOnEntity userLogOnEntity = userLogOnApp.GetForm(userEntity.F_Id);
+                    DateTime lastVisitTime = DateTime.Now;
+                    int LogOnCount = (userLogOnEntity.F_LogOnCount).ToInt() + 1;
+                    if (userLogOnEntity.F_LastVisitTime != null)
+                    {
+                        userLogOnEntity.F_PreviousVisitTime = userLogOnEntity.F_LastVisitTime.ToDate();
+                    }
+                    userLogOnEntity.F_LastVisitTime = lastVisitTime;
+                    userLogOnEntity.F_LogOnCount = LogOnCount;
+                    userLogOnApp.UpdateForm(userLogOnEntity);
+                    return userEntity;
+                }
+                else
+                {
+                    throw new Exception("账户被系统锁定,请联系管理员");
+                }
+            }
+            else
+            {
+                throw new Exception("账户不存在，请重新输入");
+            }
+        }
     }
 }
