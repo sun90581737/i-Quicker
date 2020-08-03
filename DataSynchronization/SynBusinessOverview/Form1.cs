9fb538db1e20d40e6b221e94e1f2fbcd815adabc
i-Quicker <sun905817@163.com>
2020-08-03 14:04:03 +0800

    v8.3

---
+++ DataSynchronization/SynBusinessOverview/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SynBusinessOverview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string dbnfin = GetValue("conn_nfinebase");//写入库
+        public static string EnStr = DEncrypt.Decrypt(dbnfin);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                #region 总生产成本
+
+                #endregion
+
+
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error(ex.Message);
+            }
+            finally
+            {
+                this.Close();
+            }
+        }
+        // 根据Key取Value值
+        public static string GetValue(string key)
+        {
+            return ConfigurationManager.AppSettings[key].ToString().Trim();
+        }
+    }
+}
