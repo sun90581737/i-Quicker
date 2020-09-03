c76ed6c5b7f8f1f9b42fef3d67a57838a5885721
i-Quicker <sun905817@163.com>
2020-09-03 14:52:05 +0800

    v9.3

--- NFine.Repository/AutomationLine/DataAcquisitionDetailRepository.cs
+++ NFine.Repository/AutomationLine/DataAcquisitionDetailRepository.cs
@@ -24,13 +24,15 @@ namespace NFine.Repository.AutomationLine
             {
                 Time = Convert.ToInt32(ConfTime);
             }
+            var GetDateTime= DateTime.Now.AddMinutes(-1);
             StringBuilder strSql = new StringBuilder();
-            strSql.Append(@"SELECT * FROM  nfinebase.Sys_DataAcquisitionDetail where IsEffective=1 AND RunTime between @starttime and @endtime");
+            strSql.Append(@"SELECT * FROM  nfinebase.Sys_DataAcquisitionDetail 
+            where IsEffective=1 AND RunTime between @starttime and @endtime  GROUP BY DeviceName");
             DbParameter[] parameter =
             {
                  //new SqlParameter("@account",account)
-                 new MySqlParameter("@starttime",DateTime.Now.ToString()),
-                 new MySqlParameter("@endtime",DateTime.Now.AddSeconds(Time))
+                 new MySqlParameter("@starttime",GetDateTime.ToString()),
+                 new MySqlParameter("@endtime",GetDateTime.AddSeconds(Time).ToString())
             };
             return this.FindList(strSql.ToString(), parameter);
         }
