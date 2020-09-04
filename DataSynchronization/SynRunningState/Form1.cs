0a98c5e1b60372e8fd4c9f91f4152ec9137af772
i-Quicker <sun905817@163.com>
2020-09-04 15:14:22 +0800

    v9.4

--- DataSynchronization/SynRunningState/Form1.cs
+++ DataSynchronization/SynRunningState/Form1.cs
@@ -19,12 +19,13 @@ namespace SynRunningState
         }
         public static string dbnfin = GetValue("conn_nfinebase");//写入库
         public static string EnStr = DEncrypt.Decrypt(dbnfin);
-        public string CNC1colour = GetValue("CNC1_colour");
-        public string CNC2colour = GetValue("CNC2_colour");
-        public string CNC3colour = GetValue("CNC3_colour");
-        public string CMM2colour = GetValue("CMM2_colour");
-        public string Robotcolour = GetValue("Robot_colour");
-        public string QXJcolour = GetValue("QXJ_colour");
+        public string offlinecolour = GetValue("offline_colour");
+        public string standbycolour = GetValue("standby_colour");
+        public string inoperationcolour = GetValue("inoperation_colour");
+        public string suspendcolour = GetValue("suspend_colour");
+        public string giveanalarmcolour = GetValue("giveanalarm_colour");
+        public string notreadycolour = GetValue("notready_colour");
+        public string stopitcolour = GetValue("stopit_colour");
 
         public string NotStartedcolour = GetValue("NotStarted_colour");
         public string Conductcolour = GetValue("Conduct_colour");
@@ -50,9 +51,31 @@ namespace SynRunningState
 		                MAX(CASE WHEN device_name  = '清洗机' THEN  CONCAT(device_name ,'<br>',state_name) END) AS Describe6,
 		                (SELECT group_concat((state_num)SEPARATOR ';') FROM mes_center.r02_shelf_state  WHERE shelf_name='料架A') AS Describe7,
 		                (SELECT group_concat((state_num)SEPARATOR ';') FROM mes_center.r02_shelf_state  WHERE shelf_name='料架B') AS Describe8,now(),
-                         '{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}'
+                        MAX(CASE WHEN device_name  = 'CNC1发那科' AND state_code=0 THEN '{0}'  WHEN device_name  = 'CNC1发那科' AND state_code=1 THEN '{1}'                         WHEN device_name  = 'CNC1发那科' AND state_code=2 THEN '{2}' WHEN device_name  = 'CNC1发那科' AND state_code=3 THEN '{3}'
+                        WHEN device_name  = 'CNC1发那科' AND state_code=4 THEN '{4}' WHEN device_name  = 'CNC1发那科' AND state_code=5 THEN '{5}'
+                        WHEN device_name  = 'CNC1发那科' AND state_code=6 THEN '{6}' END) AS DescribeColor1,
+                        MAX(CASE WHEN device_name  = 'CNC2发那科' AND state_code=0 THEN '{0}'  WHEN device_name  = 'CNC2发那科' AND state_code=1 THEN '{1}' 
+                        WHEN device_name  = 'CNC2发那科' AND state_code=2 THEN '{2}' WHEN device_name  = 'CNC2发那科' AND state_code=3 THEN '{3}'
+                        WHEN device_name  = 'CNC2发那科' AND state_code=4 THEN '{4}' WHEN device_name  = 'CNC2发那科' AND state_code=5 THEN '{5}'
+                        WHEN device_name  = 'CNC2发那科' AND state_code=6 THEN '{6}' END) AS DescribeColor2,
+                        MAX(CASE WHEN device_name  = 'CNC2发那科' AND state_code=0 THEN '{0}'  WHEN device_name  = 'CNC2发那科' AND state_code=1 THEN '{1}' 
+                        WHEN device_name  = 'CNC3发那科' AND state_code=2 THEN '{2}' WHEN device_name  = 'CNC2发那科' AND state_code=3 THEN '{3}'
+                        WHEN device_name  = 'CNC3发那科' AND state_code=4 THEN '{4}' WHEN device_name  = 'CNC2发那科' AND state_code=5 THEN '{5}'
+                        WHEN device_name  = 'CNC3发那科' AND state_code=6 THEN '{6}' END) AS DescribeColor3,
+                        MAX(CASE WHEN device_name  = 'CMM2海克斯康' AND state_code=0 THEN '{0}'  WHEN device_name  = 'CMM2海克斯康' AND state_code=1 THEN '{1}' 
+                        WHEN device_name  = 'CMM2海克斯康' AND state_code=2 THEN '{2}' WHEN device_name  = 'CMM2海克斯康' AND state_code=3 THEN '{3}'
+                        WHEN device_name  = 'CMM2海克斯康' AND state_code=4 THEN '{4}' WHEN device_name  = 'CMM2海克斯康' AND state_code=5 THEN '{5}'
+                        WHEN device_name  = 'CMM2海克斯康' AND state_code=6 THEN '{6}' END) AS DescribeColor4,
+                        MAX(CASE WHEN device_name  = 'Robot' AND state_code=0 THEN '{0}'  WHEN device_name  = 'Robot' AND state_code=1 THEN '{1}' 
+                        WHEN device_name  = 'Robot' AND state_code=2 THEN '{2}' WHEN device_name  = 'Robot' AND state_code=3 THEN '{3}'
+                        WHEN device_name  = 'Robot' AND state_code=4 THEN '{4}' WHEN device_name  = 'Robot' AND state_code=5 THEN '{5}'
+                        WHEN device_name  = 'Robot' AND state_code=6 THEN '{6}' END) AS DescribeColor5,
+                        MAX(CASE WHEN device_name  = '清洗机' AND state_code=0 THEN '{0}'  WHEN device_name  = '清洗机' AND state_code=1 THEN '{1}' 
+                        WHEN device_name  = '清洗机' AND state_code=2 THEN '{2}' WHEN device_name  = '清洗机' AND state_code=3 THEN '{3}'
+                        WHEN device_name  = '清洗机' AND state_code=4 THEN '{4}' WHEN device_name  = '清洗机' AND state_code=5 THEN '{5}'
+                        WHEN device_name  = '清洗机' AND state_code=6 THEN '{6}' END) AS DescribeColor6,'{7}','{8}'
 		                FROM mes_center.r01_device_state
-                )", CNC1colour, CNC2colour, CNC3colour, CMM2colour, Robotcolour, QXJcolour, NotStartedcolour + ';' + Conductcolour + ';' + Completedcolour + ';' + Abnormalcolour + ';' + Stopcolour + ';' + Cancelcolour, NotStartedcolour + ';' + Conductcolour + ';' + Completedcolour + ';' + Abnormalcolour + ';' + Stopcolour + ';' + Cancelcolour);
+                )", offlinecolour, standbycolour, inoperationcolour, suspendcolour, giveanalarmcolour, notreadycolour, stopitcolour, NotStartedcolour + ';' + Conductcolour + ';' + Completedcolour + ';' + Abnormalcolour + ';' + Stopcolour + ';' + Cancelcolour, NotStartedcolour + ';' + Conductcolour + ';' + Completedcolour + ';' + Abnormalcolour + ';' + Stopcolour + ';' + Cancelcolour);
                 int sult = ds.InsertSql(srt, out re);
                 if (sult > 0)
                 {
