1c6035f4289ee5b1a46c7601e99acabc491aa96c
i-Quicker <sun905817@163.com>
2020-12-18 09:13:29 +0800

    v12.18

---
+++ NFine.Web/Areas/AutomationLine/Controllers/VideoController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NFine.Web.Areas.AutomationLine.Controllers
+{
+    public class VideoController : ControllerBase
+    {
+
+    }
+}
