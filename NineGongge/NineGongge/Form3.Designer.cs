3ee0f985a842d445df097ea675dfa69b3a3da5db
i-Quicker <sun905817@163.com>
2021-01-12 10:37:45 +0800

    v1.12

--- NineGongge/NineGongge/Form3.Designer.cs
+++ NineGongge/NineGongge/Form3.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace NineGongge
+﻿namespace NineGongge
 {
     partial class Form3
     {
@@ -29,20 +28,92 @@ namespace NineGongge
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Controls.Add(this.panel3);
+            this.flowLayoutPanel1.Controls.Add(this.panel4);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(429, 258);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(211, 125);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(211, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(211, 125);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(0, 125);
+            this.panel3.Margin = new System.Windows.Forms.Padding(0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(211, 125);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(211, 125);
+            this.panel4.Margin = new System.Windows.Forms.Padding(0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(211, 125);
+            this.panel4.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(648, 497);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ClientSize = new System.Drawing.Size(429, 258);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form3";
             this.Text = "中航";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- NineGongge/NineGongge/Form3.cs
+++ NineGongge/NineGongge/Form3.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +19,140 @@ namespace NineGongge
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            InitColor();
+            this.Load += (s, j) =>
+            {
+                BestFitCells();
+            };
+            this.SizeChanged += (s, j) =>
+            {
+                BestFitCells();
+            };
+        }
+        string IPUrl = GetValue("IPAddress");
+        string Url1 = GetValue("Url1");
+        string Url2 = GetValue("Url2");
+        string Url3 = GetValue("Url3");
+        string Url4 = GetValue("Url4");
+        private void InitColor()
+        {
+            int i = DateTime.Now.GetHashCode();
+            foreach (Control ctrl in flowLayoutPanel1.Controls)
+            {
+                var rand = new Random(i);
+                int r = rand.Next(0, 255);
+                int g = rand.Next(0, 255);
+                int b = rand.Next(0, 255);
+                ctrl.BackColor = Color.FromArgb(255, r, g, b);
+            }
+            TempMethod(panel1, string.Format("{0}{1}", IPUrl, Url1));
+            TempMethod(panel2, string.Format("{0}{1}", IPUrl, Url2));
+            TempMethod(panel3, string.Format("{0}{1}", IPUrl, Url3));
+            TempMethod(panel4, string.Format("{0}{1}", IPUrl, Url4));
+        }
+
+        private void BestFitCells()
+        {
+            int m = 0;//margin
+            int w = flowLayoutPanel1.Width - 4 * m;
+            int y = flowLayoutPanel1.Height - 4 * m;
+
+            int fw = w / 2;
+            int fy = y / 2;
+            foreach (Control ctrl in flowLayoutPanel1.Controls)
+            {
+                ctrl.Width = fw;
+                ctrl.Height = fy;
+            }
+        }
+        public static string GetValue(string key)
+        {
+            System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (config.AppSettings.Settings[key] == null)
+                return "";
+            else
+                return config.AppSettings.Settings[key].Value;
+        }
+        public void TempMethod(Panel tempPanel, string tempUrl)
+        {
+            if (!CefSharp.Cef.IsInitialized)
+            {
+                var setting = new CefSettings
+                {
+                    Locale = "zh-CN",
+                    AcceptLanguageList = "zh-CN,zh;q=0.8",
+                    LocalesDirPath = "CHBrowser/localeDir",
+                    LogFile = "CHBrowser/LogData",
+                    PersistSessionCookies = true,
+                    UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
+                    UserDataPath = "CHBrowser/userData"
+                };
+                setting.CefCommandLineArgs.Add("disable-gpu", "1");
+                CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+                CefSharp.Cef.Initialize(setting);
+                //CefSharp.Cef.EnableHighDPISupport();
+
+            }
+            CefSharp.WinForms.ChromiumWebBrowser wb2 = new CefSharp.WinForms.ChromiumWebBrowser(string.Format("{0}{1}", IPUrl, "/FastLogin/Index?accounts=admin"));
+            wb2.Dock = DockStyle.Fill;
+            //wb2.FrameLoadEnd += webbrowser_FrameLoadEnd;//浏览器缩放
+            tempPanel.Controls.Clear();
+            tempPanel.Controls.Add(wb2);
+
+            Thread th = new Thread(() =>
+            {
+                Thread.Sleep(20000);
+                this.Invoke((Action)delegate
+                {
+                    wb2.Load(tempUrl);
+                });
+            });
+            th.Start();
+        }
+
+        const int WM_SYSCOMMAND = 0x112;
+        const int SC_CLOSE = 0xF060;
+        const int SC_MINIMIZE = 0xF020;
+        const int SC_MAXIMIZE = 0xF030;
+        const int SC_RESTORE = 61728;
+        //窗体按钮的拦截函数
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_SYSCOMMAND)
+            {
+                if (m.WParam.ToInt32() == SC_RESTORE)
+                {
+
+                }
+                if (m.WParam.ToInt32() == SC_MINIMIZE)  //拦截最小化按钮
+                {
+
+                }
+                if (m.WParam.ToInt32() == SC_MAXIMIZE)   //拦截窗体最大化按钮
+                {
+                    this.FormBorderStyle = FormBorderStyle.None;     //设置窗体为无边框样式
+                    this.WindowState = FormWindowState.Maximized;    //最大化窗体 
+                }
+                if (m.WParam.ToInt32() == SC_CLOSE)       //拦截窗体关闭按钮
+                {
+
+
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        void webbrowser_FrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+        {
+
+            ChromiumWebBrowser browser = (ChromiumWebBrowser)sender;
+
+            browser.SetZoomLevel(-3.8);
+
         }
     }
 }
